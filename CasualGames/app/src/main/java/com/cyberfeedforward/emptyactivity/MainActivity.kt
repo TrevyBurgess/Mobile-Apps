cbde5d705587c5f097ed066d1684a2078a61eb3b
Trevy Burgess <TrevyBurgess@live.com>
2026-03-26 14:42:25 -0400

    Issue # 2
    Fixed

--- CasualGames/app/src/main/java/com/cyberfeedforward/emptyactivity/MainActivity.kt
+++ CasualGames/app/src/main/java/com/cyberfeedforward/emptyactivity/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.tooling.preview.Preview
 import com.cyberfeedforward.emptyactivity.ui.AppRoot
 import com.cyberfeedforward.emptyactivity.ui.theme.EmptyActivityTheme
@@ -12,9 +14,14 @@ import com.cyberfeedforward.emptyactivity.ui.theme.EmptyActivityTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            EmptyActivityTheme {
+            val darkTheme = isSystemInDarkTheme()
+
+            SideEffect {
+                enableEdgeToEdge()
+            }
+
+            EmptyActivityTheme(darkTheme = darkTheme) {
                 AppRoot()
             }
         }
